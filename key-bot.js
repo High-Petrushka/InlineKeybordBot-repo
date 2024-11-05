5c91aecfcaa148efebed9415526d30399be3e3f7
High-Petrushka <daniilprokopovic12@gmail.com>
2024-11-05 16:19:30 -0500

    Changing the system of answers

--- key-bot.js
+++ key-bot.js
@@ -2,8 +2,26 @@ const { Bot, InlineKeyboard } = require('grammy');
 const { emojiParser } = require('@grammyjs/emoji');
 
 const bot = new Bot('7081370541:AAHbwRj2bPR2iYIYgc7OmyuvEHRGTU0TKwE');
+
 const startBtn = new InlineKeyboard().text('start testing', 'start');
-const questKeyboard = new InlineKeyboard().text('A', '1').text('B', '2').text('C', '3').text('D', '4');
+
+const firstQuest = new InlineKeyboard()
+	.text(`by Hicox's fingers gesture`, '-1').row()
+	.text(`by Stiglitz's appearance` , '-1').row()
+	.text(`by playing "Who an I?"`, '-1').row()
+	.text(`by all mentioned factors`, '1');
+
+const secondQuest = new InlineKeyboard()
+	.text(`Le Big Mac`, '-2').row()
+	.text(`Royale with Cheese`, '2').row()
+	.text(`Whopper`, '-2').row()
+	.text(`McCrispy`, '-2');
+
+const thirdQuest = new InlineKeyboard()
+	.text().row()
+	.text().row()
+	.text().row()
+	.text().row();
 
 bot.use(emojiParser());
 
@@ -15,9 +33,49 @@ bot.command('start', async (ctx) => {
 	});
 });
 
+bot.command('help', async (ctx) => {
+	const answer = ctx.emoji`/start - begin messaging\n/help - see all available options\nAlso you can use buttons, that offers the bot ${'melting_face'}`;
+	await ctx.reply(answer);
+});
+
+let totalResult = new Array;
+
 bot.callbackQuery('start', async (ctx) => {
-	await ctx.reply('First question...', {
-		reply_markup: questKeyboard,
+	await ctx.reply(`How did Dieter Hellstrom figure out\nBustard's plan in the bar scene?`, {
+		reply_markup: firstQuest,
+	});
+});
+
+bot.callbackQuery('1', async (ctx) => {
+	const answer = ctx.update.callback_query.data;
+	console.log(answer);
+
+	totalResult.push(Number(answer));
+
+	await ctx.reply('How do people in Francec call the\n"Quarter Pounder with Chees"?', {
+		reply_markup: secondQuest,
+	});
+});
+
+bot.callbackQuery('-1', async (ctx) => {
+	const answer = ctx.update.callback_query.data;
+	console.log(answer);
+
+	totalResult.push(Number(answer));
+
+	await ctx.reply('How do people in Francec call the\n"Quarter Pounder with Chees"?', {
+		reply_markup: secondQuest,
+	});
+});
+
+bot.on('callback_query:data', async (ctx) => {
+	console.log('Unknown button event with payload', ctx.callbackQuery.data);
+	await ctx.answerCallbackQuery(); // remove loading animation
+});
+
+bot.on('message', async (ctx) => {
+	await ctx.reply('Please, use /help command to see all\navailable options in terms of interaction the with bot', {
+		reply_parameters: {message_id: ctx.update.message.message_id},
 	});
 });
 
